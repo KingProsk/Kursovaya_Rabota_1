563949047370c0d12bea7ab522854b0e919d9dc8
Илья <king.prosk@gmail.com>
2024-02-08 22:59:42 +0300

    Kursovaya_Rabota_1
    Kursovaya_Rabota_1

---
+++ Kursovaya_Proskyrnich/Kursovaya_Proskyrnich/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Kursovaya_Proskyrnich
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Term_1 = new System.Windows.Forms.Button();
+            this.Term_2 = new System.Windows.Forms.Button();
+            this.Otgruzka = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Term_1
+            // 
+            this.Term_1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Term_1.Location = new System.Drawing.Point(12, 330);
+            this.Term_1.Name = "Term_1";
+            this.Term_1.Size = new System.Drawing.Size(238, 52);
+            this.Term_1.TabIndex = 1;
+            this.Term_1.Text = "Терминал №1";
+            this.Term_1.UseVisualStyleBackColor = true;
+            this.Term_1.Click += new System.EventHandler(this.Term_1_Click);
+            // 
+            // Term_2
+            // 
+            this.Term_2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Term_2.Location = new System.Drawing.Point(549, 330);
+            this.Term_2.Name = "Term_2";
+            this.Term_2.Size = new System.Drawing.Size(238, 52);
+            this.Term_2.TabIndex = 1;
+            this.Term_2.Text = "Терминал №2";
+            this.Term_2.UseVisualStyleBackColor = true;
+            this.Term_2.Click += new System.EventHandler(this.Term_2_Click);
+            // 
+            // Otgruzka
+            // 
+            this.Otgruzka.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Otgruzka.Location = new System.Drawing.Point(256, 388);
+            this.Otgruzka.Name = "Otgruzka";
+            this.Otgruzka.Size = new System.Drawing.Size(288, 50);
+            this.Otgruzka.TabIndex = 1;
+            this.Otgruzka.Text = "Отгрузить";
+            this.Otgruzka.UseVisualStyleBackColor = true;
+            this.Otgruzka.Click += new System.EventHandler(this.Otgruzka_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(256, 362);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(288, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(256, 330);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(287, 26);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(549, 148);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(238, 36);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 148);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(238, 36);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(797, 455);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Term_2);
+            this.Controls.Add(this.Otgruzka);
+            this.Controls.Add(this.Term_1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Term_1;
+        private System.Windows.Forms.Button Term_2;
+        private System.Windows.Forms.Button Otgruzka;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Kursovaya_Proskyrnich/Kursovaya_Proskyrnich/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kursovaya_Proskyrnich
+{   
+    public partial class Form1 : Form
+    {
+        public Graphics g;                
+        public Deque deque;
+        public Font font;         
+        public Form1()
+        {
+            InitializeComponent();            
+            ClientSize = new Size(813, 494); 
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.Text = "СКЛАД";
+            textBox1.MaxLength = 4;
+            deque = new Deque();
+            g = this.CreateGraphics();                     
+            font = new Font("Times New Roman", 14); // шрифт для надписей на форме
+            Bitmap BM = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics h = Graphics.FromImage(BM);
+            h.DrawString("Введите ID Груза: ", font, SystemBrushes.WindowText, new PointF(10,0));
+            pictureBox1.Image = BM;
+            Bitmap BM2 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            Graphics h2 = Graphics.FromImage(BM2);
+            h2.DrawString("Терминал №2", font, SystemBrushes.WindowText, new PointF(10, 0));
+            pictureBox2.Image = BM2;
+            Bitmap BM3 = new Bitmap(pictureBox3.Width, pictureBox3.Height);
+            Graphics h3 = Graphics.FromImage(BM3);
+            h3.DrawString("Терминал №1", font, SystemBrushes.WindowText, new PointF(120, 0));
+            pictureBox3.Image = BM3;
+            
+        }       
+        private void MyDraw(Deque d) // функция для отрисовки содержимого Deque
+        {
+            font = new Font("Times New Roman", 20); // шрифт для отрисовки ID грузов
+            g.Clear(Color.White);
+            string str;
+            SizeF MessSize;
+            PointF p = new PointF((ClientSize.Width) / 2, (ClientSize.Height) / 2); // координаты середины формы
+            g.DrawLine(new Pen(Color.Red, 3), p.X, p.Y + 50, p.X, p.Y - 10);
+            for (int i = d.index - 1; i >= 0; i--) // цикл отрисовки для левой стороны Дека
+            {
+                str = d.elements[i].ToString();
+                MessSize = g.MeasureString(str, font);
+                p.X = p.X - MessSize.Width - 10;
+                g.DrawString(str, font, SystemBrushes.WindowText, p);
+            }
+            p = new PointF((ClientSize.Width) / 2, (ClientSize.Height) / 2);
+            for (int i = d.index + 1; i < d.Size(); i++) // цикл отрисовки для правой стороны дека
+            {
+                str = d.elements[i].ToString();
+                MessSize = g.MeasureString(str, font);
+                if (i == d.index + 1)
+                {
+                    p.X = p.X + 10;
+                }
+                else
+                {
+                    p.X = p.X + MessSize.Width + 10;
+                }
+                g.DrawString(str, font, SystemBrushes.WindowText, p);
+            }
+        }
+        private void Term_1_Click(object sender, EventArgs e) // при нажатии на кнопку, добавляет ID, с текстового поля, в начало Дека 
+        {
+            if (textBox1.Text.Length < 4) // ограничение на ввод символов
+            {
+                MessageBox.Show("Номер груза должен состоять из 4 - х символов!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }          
+            else
+            {
+                deque.Push_Front(textBox1.Text); // применение метода "вставить в начало"
+                MyDraw(deque); // отрисовка Дека на форме                
+                textBox1.Clear(); // очистка текст бокса
+            }
+        }
+        private void Term_2_Click(object sender, EventArgs e) // при нажатии на кнопку, добавляет ID, с текстового поля, в конец Дека
+        {            
+            if (textBox1.Text.Length < 4)
+            {
+                MessageBox.Show("Номер груза должен состоять из 4 - х символов!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }                    
+            else
+            {
+                deque.Push_Back(textBox1.Text); // применение метода "вставить в конец"
+                MyDraw(deque);
+                textBox1.Clear();
+            }
+        }
+        private void Otgruzka_Click(object sender, EventArgs e) // при нажатии на кнопку, удаляет, введённый в текстовое поле, ID груза, помещает все ID, находящиеся перед ним, в Стек, и возвращает после удаления этого ID
+        {
+            if (textBox1.Text.Length < 4)
+            {
+                MessageBox.Show("Номер груза должен состоять из 4 - х символов!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                deque.Remove_Element(textBox1.Text); // применение метода "удалить ID"
+                MyDraw(deque);
+                textBox1.Clear();
+            }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) // запрет на ввод пробеля в текст бокс
+        {         
+            if(e.KeyChar == 32)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+    public class Deque // создание класса Дек
+    {
+        public ArrayList elements;  // динамический массив 
+        public string key; 
+        public int index;
+        public Stack<string> memory; // Стек
+        public Deque() // конструктор класса
+        {
+            elements = new ArrayList();
+            key = "*"; // элемент, разделяющий левую и правую часть Дека
+            elements.Add(key);
+            index = 0;           
+        }
+        public void Remove_Element(string element) // метод удаления элемента 
+        {                       
+            memory = new Stack<string>();
+            if(elements.Contains(element) == true)
+            {                
+                if (elements.IndexOf(element) < index)
+                {
+                    while (elements[0].ToString() != element) // для левой части Дека 
+                    {
+                        memory.Push(elements[0].ToString()); // добавление элементов, "мешающих" изъятию заданного, в Стек
+                        this.Pop_Front();// удаление элемента из начала дека
+                    }
+                    this.Pop_Front(); // удаление заданного элемента из начала дека
+                    while (memory.Count > 0)
+                    {
+                        this.Push_Front(memory.Pop()); // возвращение "мешающих" изъятию элементов, обратно в начало Дека
+                    }
+                }
+                else
+                {
+                    while (elements[elements.Count - 1].ToString() != element) // для правой части Дека
+                    {
+                        memory.Push(elements[elements.Count - 1].ToString());
+                        this.Pop_Back(); // удаление элемента из конца Дека
+                    }
+                    this.Pop_Back(); // удаление заданного элемента из конца дека
+                    while (memory.Count > 0)
+                    {
+                        this.Push_Back(memory.Pop()); // возвращение "мешающих" изъятию элементов, обратно в конец Дека
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Груз отсутствует на складе!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        public void Push_Back(string element) // метод добавления элемента в конец Дека
+        {
+            if (elements.Contains(element) == false && element != "*") 
+            {
+                elements.Add(element);
+            }
+            else
+            {
+                MessageBox.Show("Груз с таким номером уже находится на складе!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        public string Pop_Back() // метод удаления элемента с конеца Дека
+        {
+            string result = (string)elements[elements.Count - 1];
+            elements.RemoveAt(elements.Count - 1);
+            return result;
+        }
+        public void Push_Front(string element) // метод добавления элемента в начало Дека
+        {            
+            if (elements.Contains(element) == false)
+            {
+                elements.Insert(0, element);
+                index++;
+            }
+            else
+            {
+                MessageBox.Show("Груз с таким номером уже находится на складе!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        public string Pop_Front() // метод удаления елемента с начала Дека
+        {
+            string result = (string)elements[0];
+            elements.RemoveAt(0);
+            index--;
+            return result;
+        }
+        public int Size() // метод, возвращающий размер Дека
+        {
+            return elements.Count;
+        }     
+    }
+}
